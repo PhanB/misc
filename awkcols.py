57fa2a3071c64cdbd6a3ef09cb99b6729aa765bd
PhanB <bailey.phan@uky.edu>
2017-09-07 10:02:11 -0400

    Awkcols accepts filename and headers as input.
    Checks to see if file opens.
    Omits headers with invalid characters.

--- awkcols.py
+++ awkcols.py
@@ -1,25 +1,47 @@
 #Author: Bailey Phan
-#Date: September 5, 2017
+#Date: September 7, 2017
 #Purpose: Print out columns of a csv with matching headers
 
-#TODO: take in arguments (first = filename, rest = column headers)
-
 import subprocess
+import sys
+import re
 
 def main():
-    filename = 'my_file.csv' #file to search
-    column_headers = ['testing', 'one', 'two', 'three', 'sample_id'] #columns to print out
 
-    #open file
-    f = open(filename, 'r')
-    line = f.readline().split(',')
+    #get file and headers of columns to print
+    filename = sys.argv[1] #file to search
+    column_headers = [] #columns to print out
+    invalid_headers = [] #headers with invalid characters 
+
+    #check inputted headers
+    an = re.compile('^[\w\s\-\/\\!@#$%^&*()_\{\}~\+]+$') #accepts alphanumeric chars, spaces, and symbols
+    for arg in sys.argv[2:]:
+        if an.match(arg):
+            column_headers.append(arg)
+        else:
+            invalid_headers.append(arg)
+
+    #if no column headers were accepted don't run program
+    if len(column_headers) < 1:
+        print_invalid_headers(invalid_headers)
+        print("No valid column headers to search for.")
+        return 1
+
+
+    #open file and tokenize first line of it
+    try:
+        f = open(filename, 'r')
+        first_line = f.readline().split(',')
+    except IOError:
+        print("Unable to open file: " + filename)
+        return 1
     
     #create bash command (awk columns which match elements in column_headers)
     bashcommand = ['awk', '-F\',\'']
     awkcmd = '{ print '
     counter = 1
 
-    for word in line:
+    for word in first_line:
         #if we find a header that was inputted, awk this column
         if word in column_headers:
             awkcmd = awkcmd + '$' + str(counter) + '"   "'
@@ -39,7 +61,15 @@ def main():
 
     #if column headers were inputted that were not found, tell them
     if(len(column_headers) > 0):
+        print_invalid_headers(invalid_headers)
         print("Column header(s) not found: " + str(column_headers))
+
+    return 0
+
+
+def print_invalid_headers(invalid_list):
+    if len(invalid_list) > 0:
+        print("Invalid headers: " + str(invalid_list))
 
 if __name__ == '__main__':
     main()
